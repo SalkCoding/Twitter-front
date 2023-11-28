ea74be6d664496826260ec0e032ec67dcc5a7478
Sangbeom Jo <josangbeom1104@gmail.com>
2023-11-28 15:55:38 +0900

    Favicon added

--- src/main/java/com/salkcdoing/twitter/twitter/HelloApplication.java
+++ src/main/java/com/salkcdoing/twitter/twitter/HelloApplication.java
@@ -2,13 +2,14 @@ package com.salkcdoing.twitter.twitter;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage stage) {
         // WebView 인스턴스 생성
         WebView webView = new WebView();
 
@@ -17,12 +18,15 @@ public class HelloApplication extends Application {
         engine.setJavaScriptEnabled(true);
         engine.load("http://localhost:8080");
 
+        stage.getIcons().add(new Image("/icon/twitter.png"));
+        stage.setTitle("Twitter");
+
         // Scene 생성 및 WebView 추가
         Scene scene = new Scene(webView, 800, 600);
 
         // Stage 설정
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
